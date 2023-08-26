719a479119697b04936d0c1543cf9a77b6d5ce49
Markley Mendes <markley.mendes@gmail.com>
2023-08-26 16:48:10 +0000

    refactor: improve ccode

--- src/app/new-route/page.tsx
+++ src/app/new-route/page.tsx
@@ -5,47 +5,52 @@ import { FormEvent, useRef, useState } from "react";
 import { useMap } from "../hooks/userMap";
 
 export function NewRoutePage() {
-
   const mapContainerRef = useRef<HTMLDivElement>(null);
   const map = useMap(mapContainerRef);
-  const [directionsData, setDirectionsData] = useState<DirectionsResponseData & { request: any }>();
+  const [directionsData, setDirectionsData] = useState<
+    DirectionsResponseData & { request: any }
+  >();
 
   async function searchPlaces(event: FormEvent) {
     event.preventDefault();
-    const source = (document.getElementById("source") as HTMLInputElement).value;
-    const destination = (document.getElementById("destination") as HTMLInputElement).value;
+    const source = (document.getElementById("source") as HTMLInputElement)
+      .value;
+    const destination = (
+      document.getElementById("destination") as HTMLInputElement
+    ).value;
 
     const [sourceResponse, destinationResponse] = await Promise.all([
       fetch(`http://localhost:3000/places?text=${source}`),
       fetch(`http://localhost:3000/places?text=${destination}`),
     ]);
 
-    const [sourcePlace, destinationPlace]: FindPlaceFromTextResponseData[] = await Promise.all([
-      sourceResponse.json(),
-      destinationResponse.json(),
-    ]);
+    const [sourcePlace, destinationPlace]: FindPlaceFromTextResponseData[] =
+      await Promise.all([sourceResponse.json(), destinationResponse.json()]);
 
     if (sourcePlace.status !== "OK") {
-      alert("Não foi possivel carregar a origem");
+      console.error(sourcePlace);
+      alert("Não foi possível encontrar a origem");
       return;
     }
+
     if (destinationPlace.status !== "OK") {
-      alert("Não foi possivel carregar o destino");
+      console.error(destinationPlace);
+      alert("Não foi possível encontrar o destino");
       return;
     }
 
     const placeSourceId = sourcePlace.candidates[0].place_id;
     const placeDestinationId = destinationPlace.candidates[0].place_id;
 
-    const directionsResponse = await fetch(`http://localhost:3000/directions?originId=${placeSourceId}&destinationId=${placeDestinationId}`);
-    const directionsData: DirectionsResponseData & { request: any } = await directionsResponse.json();
-
+    const directionsResponse = await fetch(
+      `http://localhost:3000/directions?originId=${placeSourceId}&destinationId=${placeDestinationId}`
+    );
+    const directionsData: DirectionsResponseData & { request: any } =
+      await directionsResponse.json();
     setDirectionsData(directionsData);
-
     map?.removeAllRoutes();
-
     await map?.addRouteWithIcons({
-      routeId: '1',
+      routeId: "1",
       startMarkerOptions: {
         position: directionsData.routes[0].legs[0].start_location,
       },
@@ -54,8 +59,8 @@ export function NewRoutePage() {
       },
       carMarkerOptions: {
         position: directionsData.routes[0].legs[0].start_location,
-      }
-    })
+      },
+    });
   }
 
   async function createRoute() {
@@ -64,7 +69,7 @@ export function NewRoutePage() {
     const response = await fetch('http://localhost:3000/routes', {
       method: 'POST',
       headers: {
-        'Content-Type': 'aplication/json'
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         name: `${startAddress} - ${endAddress}`,
@@ -76,10 +81,20 @@ export function NewRoutePage() {
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', height: '100%', width: '100%' }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        height: "100%",
+        width: "100%",
+      }}
+    >
       <div>
-        <h1>Nova Rota</h1>
-        <form style={{ display: "flex", flexDirection: "column" }} onSubmit={searchPlaces}>
+        <h1>Nova rota</h1>
+        <form
+          style={{ display: "flex", flexDirection: "column" }}
+          onSubmit={searchPlaces}
+        >
           <div>
             <input id="source" type="text" placeholder="origem" />
           </div>
@@ -90,15 +105,22 @@ export function NewRoutePage() {
         </form>
         {directionsData && (
           <ul>
-            <li>Origem: {directionsData!.routes[0].legs[0].start_address}</li>
-            <li>Destino: {directionsData!.routes[0].legs[0].end_address}</li>
+            <li>Origem {directionsData.routes[0].legs[0].start_address}</li>
+            <li>Destino {directionsData.routes[0].legs[0].end_address}</li>
             <li>
-              <button onClick={ createRoute }>Criar rota</button>
+              <button onClick={createRoute}>Criar rota</button>
             </li>
           </ul>
         )}
       </div>
-      <div id="map" style={{ height: '100%', width: '100%' }} ref={mapContainerRef}></div>
+      <div
+        id="map"
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
+        ref={mapContainerRef}
+      ></div>
     </div>
   );
 }
