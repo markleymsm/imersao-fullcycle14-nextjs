3a2b43b19cb4756efd35e6fb7d46260a2d9c92e2
Markley Mendes <markley.mendes@gmail.com>
2023-08-26 11:57:28 +0000

    bug: adjustment userMap

--- src/app/hooks/userMap.ts
+++ src/app/hooks/userMap.ts
@@ -6,21 +6,20 @@ import { Map } from "../utils/map";
 export function useMap(containerRef: React.RefObject<HTMLDivElement>) {
   const [map, setMap] = useState<Map>();
 
-
   useEffect(() => {
     (async () => {
       const loader = new Loader({
         apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-        libraries: ['routes', 'geometry']
+        libraries: ["routes", "geometry"],
       });
-      await Promise.all([
+      const [, , position] = await Promise.all([
         loader.importLibrary("routes"),
-        loader.importLibrary("routes"),
-        getCurrentPosition({ enableHighAccuracy: true })
+        loader.importLibrary("geometry"),
+        getCurrentPosition({ enableHighAccuracy: true }),
       ]);
       const map = new Map(containerRef.current!, {
         zoom: 15,
-        center: { lat: -23.5505, lng: -49.6333 }
+        center: position,
       });
       setMap(map);
     })();
